48ced6a2473313f7dc2143b1bba56362359896b0
Yonas Assefa <se.yonas.assefa@gmail.com>
2024-09-16 08:45:07 +0300

    fix: add job form created

--- myproject/settings.py
+++ myproject/settings.py
@@ -40,8 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'jobs',
     'widget_tweaks',
-    'jobs'
 ]
 
 MIDDLEWARE = [
